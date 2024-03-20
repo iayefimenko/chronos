9ec866753f26d80eafd6b693b9325b24ca91bdfc
Anastasiia Yefimenko <105549011+iayefimenko@users.noreply.github.com>
2024-03-20 23:25:36 +0200

    Create auth.js

---
+++ src/api/routes/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const authController = require("../controllers/auth");
+
+const vSchema = require("../validations/auth");
+const { validate } = require("../middlewares/validate");
+
+const router = express.Router();
+
+router.post("/signup", validate(vSchema.Signup), authController.signup);
+router.post("/login", validate(vSchema.Login), authController.login);
+router.post(
+  "/verify-email",
+  validate(vSchema.TokenVerification),
+  authController.verifyEmail
+);
+
+router.post(
+  "/reset-password",
+  validate(vSchema.ResetPassword),
+  authController.resetPassword
+);
+
+router.post(
+  "/confirm-password-reset",
+  validate(vSchema.ResetPasswordConf),
+  authController.confirmPasswordReset
+);
+
+router.post("/refresh-token", authController.refreshToken);
+router.post("/logout", authController.logout);
+
+module.exports = router;
